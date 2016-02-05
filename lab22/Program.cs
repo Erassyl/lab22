8d3411c49ee7ef5d93a50f56159bc9150ec2dc0c
Erassyl <erasylzhanabai@mail.ru>
2016-02-05 22:10:38 +0600

    lab22

---
+++ lab22/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sw = new StreamReader(fs);
+            string s = sw.ReadToEnd();
+            sw.Close();
+            fs.Close();
+            string[] array = s.Split(' ');
+            int max = -10000;
+            int min = 10000;
+            for(int i=0; i<array.Length; i++)
+            {
+                int number = int.Parse(array[i]);
+                if(number>max)
+                {
+                    max = number;
+                }   
+                if (number<min)
+                {
+                    min = number;
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
